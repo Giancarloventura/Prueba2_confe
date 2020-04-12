4316e5dc76ee03d296f79748e18b404487c930b6
Giancarlo Ventura Haro <g.ventura@pucp.pe>
2020-04-12 15:19:31 -0500

    primer commit

---
+++ Problema1/src/main/java/com/mycompany/problema1/Holamundo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.problema1;
+import java.util.Scanner;
+/**
+ *
+ * @author g84131982
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("bebe");
+        String cadena;
+        
+        
+        int n;
+            System.out.println("GIAN CARLO ES KBRO?");
+                    
+             cadena = sc.nextLine();
+             //System.out.println(cadena+"*");
+             if(cadena.equals("SI")){
+                System.out.println("CLARO PES ESE PERRO ES MAS KABRO QUE RICK Y JOSÉ :3 JUNTOS");}
+             else{
+                 System.out.println("NO PES GIL SOLO COMO COLOCHITAS MIAU :3"); 
+       
+             }
+       
+        
+    }
+    
+}
